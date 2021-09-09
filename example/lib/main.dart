1321873f6da5d5004c2c890adc5a43342851b930
deimos <marcobavagnolidev@gmail.com>
2021-09-09 22:39:08 +0200

    computation is now on a isolate. Added luminance

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,8 @@
 import 'dart:typed_data';
-import 'dart:ui' as ui;
-import 'package:image/image.dart' as image;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:async';
-
+import 'package:image/image.dart' as image;
 import 'package:median_cut/median_cut.dart';
 
 void main() {
@@ -21,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   final stopwatch = Stopwatch();
   Duration elapsed = Duration.zero;
   List<Palette> palette = [];
-  int numPaletteColors = 8;
+  int numPaletteColors = 10;
 
   @override
   Widget build(BuildContext context) {
@@ -36,54 +33,93 @@ class _MyAppState extends State<MyApp> {
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-
                 SizedBox(
-                  width: 300,
-                  height: 300,
-                  child: Image.asset('assets/landscape-scene.jpeg', fit: BoxFit.scaleDown)
+                    width: 300,
+                    height: 300,
+                    child: Image.asset('assets/landscape-scene.jpeg',
+                        fit: BoxFit.scaleDown)),
+
+                /// buttons
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        stopwatch.start();
+                        final ByteData assetImageByteData = await rootBundle
+                            .load('assets/landscape-scene.jpeg');
+                        image.Image? baseSizeImage = image.decodeImage(
+                            assetImageByteData.buffer.asUint8List());
+                        Uint8List imgBuffer = baseSizeImage!.getBytes();
+
+                        MedianCut.medianCut(numPaletteColors, imgBuffer)
+                            .then((value) {
+                          palette = value;
+                          setState(() {
+                            elapsed = stopwatch.elapsed;
+                            stopwatch.stop();
+                            stopwatch.reset();
+                          });
+                        });
+                      },
+                      child: Text('get palette'),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          palette.sort((a, b) {
+                            return a.luminance > b.luminance
+                                ? 1
+                                : a.luminance == b.luminance
+                                    ? 0
+                                    : -1;
+                          });
+                          setState(() {});
+                        },
+                        child: Text('luminance sort')),
+                    ElevatedButton(
+                        onPressed: () {
+                          palette.sort((a, b) {
+                            return a.percentage > b.percentage
+                                ? 1
+                                : a.percentage == b.percentage
+                                    ? 0
+                                    : -1;
+                          });
+                          setState(() {});
+                        },
+                        child: Text('% sort'))
+                  ],
                 ),
 
-                ElevatedButton(
-                  onPressed: () async{
-                    stopwatch.start();
-                    final ByteData assetImageByteData = await rootBundle.load('assets/landscape-scene.jpeg');
-                    image.Image? baseSizeImage = image.decodeImage(assetImageByteData.buffer.asUint8List());
-                    Uint8List imgBuffer = baseSizeImage!.getBytes();
+                /// draw samples color values
+                if (palette.length > 0) Text('Elapsed: $elapsed'),
 
-                    palette = MedianCut().medianCut(numPaletteColors, imgBuffer);
-                    setState(() {
-                      elapsed = stopwatch.elapsed;
-                      stopwatch.stop();
-                      stopwatch.reset();
-                    });
-                  },
-                  child: Text('get palette'),
-                ),
-
-                if (palette.length > 0)
-                  Text('Elapsed: $elapsed'),
-
+                /// draw samples colors
                 if (palette.length > 0)
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Column(
-                      children: List.generate(numPaletteColors, (index) =>
-                          Row(
-                            children: [
-                              Container(
-                                width: 60,
-                                height: 30,
-                                color: palette.elementAt(index).color,
-                              ),
-                              SizedBox(width: 10),
-                              Text('${palette.elementAt(index).color}   '
-                                  '${palette.elementAt(index).percentage.toStringAsFixed(2)}%'),
-                            ],
-                          )
-                      ),
+                      children: List.generate(numPaletteColors, (index) {
+                        String color =
+                            palette.elementAt(index).color.toString();
+                        String percentage = palette
+                            .elementAt(index)
+                            .percentage
+                            .toStringAsFixed(2);
+                        return Row(
+                          children: [
+                            Container(
+                              width: 60,
+                              height: 30,
+                              color: palette.elementAt(index).color,
+                            ),
+                            SizedBox(width: 10),
+                            Text('$color  $percentage%'),
+                          ],
+                        );
+                      }),
                     ),
                   )
-
               ],
             ),
           ),
--- lib/median_cut.dart
+++ lib/median_cut.dart
@@ -1,85 +1,86 @@
-
+import 'dart:async';
 import 'dart:ffi';
 import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:ffi/ffi.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
+// Ref. http://palette-maker.herokuapp.com/index.html
+
+/// Palette class
 class Palette {
   Color color;
+  double luminance;
   double percentage;
 
-  Palette(this.color, this.percentage);
+  Palette(this.color, this.luminance, this.percentage);
+}
+
+/// isolate callback
+Future<List<Palette>> _isolateCallBack(Map<int, dynamic> args) {
+  DynamicLibrary nativeLib = Platform.isAndroid
+      ? DynamicLibrary.open("libmedian_cut.so")
+      : DynamicLibrary.process();
+
+  // C decl   void getPalette(int32_t numColors, int8_t *imgBuffer, int64_t imgBufferLength, int8_t *palette)
+  var getPaletteFfi = nativeLib
+      .lookup<
+          NativeFunction<
+              Pointer<Void> Function(
+                  Int32 numColors,
+                  Pointer<Uint8> imgBuffer,
+                  Int64 imgBufferLength,
+                  Pointer<Uint8> palette,
+                  Pointer<Double> percentages)>>('getPalette')
+      .asFunction<
+          Pointer<Void> Function(
+              int numColors,
+              Pointer<Uint8> imgBuffer,
+              int imgBufferLength,
+              Pointer<Uint8> palette,
+              Pointer<Double> percentages)>();
+
+  int numColors = args[0];
+  Uint8List imgBuffer = args[1];
+  Completer<List<Palette>> completer = Completer<List<Palette>>();
+  List<Palette> ret = [];
+  Pointer<Uint8> buffer = calloc<Uint8>(imgBuffer.length);
+  Pointer<Uint8> palette = calloc<Uint8>(numColors * 4);
+  Pointer<Double> percentages = calloc<Double>(numColors * 8);
+
+  for (var i = 0; i < imgBuffer.length; i++) {
+    buffer.elementAt(i).value = imgBuffer[i];
+  }
+
+  getPaletteFfi(numColors, buffer, imgBuffer.length, palette, percentages);
+
+  for (int i = 0; i < numColors; i++) {
+    Color c = Color.fromARGB(
+        palette.elementAt(i * 4 + 3).value,
+        palette.elementAt(i * 4 + 2).value,
+        palette.elementAt(i * 4 + 1).value,
+        palette.elementAt(i * 4).value);
+    ret.add(Palette(c, c.computeLuminance(), percentages.elementAt(i).value));
+  }
+
+  calloc.free(buffer);
+  calloc.free(palette);
+  calloc.free(percentages);
+
+  completer.complete(ret);
+
+  return completer.future;
 }
 
 class MedianCut {
-  late DynamicLibrary _nativeLib;
-  var _getPaletteFfi;
-
-
-  static final MedianCut _instance = MedianCut._privateConstructor();
-
-  factory MedianCut() {
-    return _instance;
-  }
-  MedianCut._privateConstructor() {
-    _nativeLib = Platform.isAndroid
-        ? DynamicLibrary.open("libmedian_cut.so")
-        : DynamicLibrary.process();
-
-    // C decl   void getPalette(int32_t numColors, int8_t *imgBuffer, int64_t imgBufferLength, int8_t *palette)
-    _getPaletteFfi = _nativeLib
-        .lookup<
-            NativeFunction<
-                Pointer<Void> Function(
-                    Int32 numColors,
-                    Pointer<Uint8> imgBuffer,
-                    Int64 imgBufferLength,
-                    Pointer<Uint8> palette,
-                    Pointer<Double> percentages)>>('getPalette')
-        .asFunction<
-            Pointer<Void> Function(
-                int numColors,
-                Pointer<Uint8> imgBuffer,
-                int imgBufferLength,
-                Pointer<Uint8> palette,
-                Pointer<Double> percentages)>();
-  }
-
-  List<Palette> medianCut(int numColors, Uint8List imgBuffer) {
-    List<Palette> ret = [];
-    Pointer<Uint8> buffer = uint8ListToPointer(imgBuffer);
-    Pointer<Uint8> palette = calloc<Uint8>(numColors * 4);
-    Pointer<Double> percentages = calloc<Double>(numColors*8);
-
-    _getPaletteFfi(numColors, buffer, imgBuffer.length, palette, percentages);
-
-    for (int i=0; i<numColors; i++) {
-      ret.add(
-        Palette(
-          Color.fromARGB(
-            palette.elementAt(i*4+3).value,
-            palette.elementAt(i*4+2).value,
-            palette.elementAt(i*4+1).value,
-            palette.elementAt(i*4).value
-          ),
-          percentages.elementAt(i).value
-      ));
-    }
-
-    calloc.free(buffer);
-    calloc.free(palette);
-    calloc.free(percentages);
-    return ret;
-  }
-
-  Pointer<Uint8> uint8ListToPointer(Uint8List list) {
-    final Pointer<Uint8> ptr = calloc<Uint8>(list.length);
-    for (var i = 0; i < list.length; i++) {
-      ptr.elementAt(i).value = list[i];
-    }
-    return ptr;
-  }
-
+  /// Compute image median-cut palette
+  ///
+  /// [numcolors]: number of colors to be computed
+  /// [imgBuffer]: raw image pixels data. Colors sequence must be in rgba format
+  ///
+  static Future<List<Palette>> medianCut(
+          int numColors, Uint8List imgBuffer) async =>
+      compute(_isolateCallBack, {0: numColors, 1: imgBuffer});
 }
